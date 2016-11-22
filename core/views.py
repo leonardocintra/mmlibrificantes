5049d2c2e943914fc1c47660004ea1768d364834
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-22 12:22:55 -0200

    inserido as url

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class ContatoView(TemplateView):
+    template_name = 'contato.html'
+
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
+
+
+index = IndexView.as_view()
+contato = ContatoView.as_view()
+sobre = SobreView.as_view()
